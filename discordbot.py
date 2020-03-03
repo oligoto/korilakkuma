2e2414a78f46bcdd7018209e7f29c0783168f7d4
oligoto <61726719+oligoto@users.noreply.github.com>
2020-03-03 23:34:28 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -10,10 +10,10 @@ async def on_voice_state_update(member, before, after):
     now = datetime.utcnow() + timedelta(hours=9)
     alert_channel = client.get_channel(684293450954178585)
     if before.channel is None:
-        msg = f'{now:%m/%d-%H:%M}に{member.name}さんがおべんきょうを始めました。がんばって！'
+        msg = f'{now:%m/%d-%H:%M}から{member.name}さんがお勉強を始めました。がんばって！'
         await alert_channel.send(msg)
     elif after.channel is None:
-        msg = f'{now:%m/%d-%H:%M}に{member.name}さんお疲れさまでした！'
+        msg = f'{now:%m/%d-%H:%M}まで{member.name}さんがお勉強しました。お疲れさまでした！'
         await alert_channel.send(msg)
 
 client.run(token)
