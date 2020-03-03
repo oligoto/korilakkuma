4c199d053a6fe7c8b40fe1ba7756f9b3b022b0dc
oligoto <61726719+oligoto@users.noreply.github.com>
2020-03-03 17:46:58 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,22 +1,18 @@
 import discord
-import os
-import datetime
+from datetime import datetime, timedelta
 
 token = os.environ['DISCORD_BOT_TOKEN']
 client = discord.Client()
 
 @client.event
-async def on_voice_state_update(before, after):
-    nowtime = datetime.datetime.utcnow()
-    nowtime = nowtime + datetime.timedelta(hours=9)
-    nowtime = nowtime.strftime("%m/%d-%H:%M")
-    vcchannel = client.get_channel('time')
-
-if(before.voice_channel is None):
-    jointext=nowtime + "に　" + after.name + "　が　" + after.voice_channel.name + " に参加しました。"
-    await client.send_message(vcchannel, jointext)
-elif(after.voice_channel is None):
-    outtext=nowtime + "に " + before.name + "　が　" + before.voice_channel.name + " から退出しました。"
-    await client.send_message(vcchannel, outtext)
+async def on_voice_state_update(member, before, after): 
+        now = datetime.utcnow() + timedelta(hours=9)
+        alert_channel = client.get_channel(time)
+        if before.channel is None: 
+            msg = f'{now:%m/%d-%H:%M} に {member.name} が {after.channel.name} に参加しました。'
+            await alert_channel.send(msg)
+        elif after.channel is None: 
+            msg = f'{now:%m/%d-%H:%M} に {member.name} が {before.channel.name} から退出しました。'
+            await alert_channel.send(msg)
 
 client.run(token)
