df1836e097595717752ca53d603ee1090b4de594
oligoto <61726719+oligoto@users.noreply.github.com>
2020-03-03 18:09:12 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -4,15 +4,15 @@ import os
 
 token = os.environ['DISCORD_BOT_TOKEN']
 client = discord.Client()
+pretime_dict = {}
 
 @client.event
 async def on_voice_state_update(member, before, after):
-    now = datetime.utcnow() + timedelta(hours=9)
     alert_channel = client.get_channel(684293450954178585)
     if before.channel is None:
-        
+
         pretime_dict[after.name] = datetime.datetime.now()
-        
+
         msg = f'{member.name} さんがお勉強を始めました。がんばって！'
         await alert_channel.send(msg)
     elif after.channel is None:
