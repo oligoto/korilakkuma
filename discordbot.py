6a2e29e47489dda82efe5aaa1a6614dbb4953112
oligoto <61726719+oligoto@users.noreply.github.com>
2020-03-03 17:26:23 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,37 +1,23 @@
 from discord.ext import commands
+import discord 
 import os
-import traceback
 import datetime
 
-bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 client = discord.Client()
-pretime_dict = {}
+ 
+@client.event 
+async def on_voice_state_update(before, after): 
+    if nowtime = datetime.datetime.utcnow() 
+        nowtime = nowtime + datetime.timedelta(hours=9) 
+        nowtime = nowtime.strftime("%m/%d-%H:%M") 
+        vcchannel = client.get_channel('time') 
+ 
+        if(before.voice_channel is None): 
+            jointext=nowtime + "に　"+ after.name + "　が　"+ after.voice_channel.name + " に参加しました。" 
+            await client.send_message(vcchannel, jointext) 
+        elif(after.voice_channel is None): 
+            outtext=nowtime + "に　"+ before.name + "　が　"+ before.voice_channel.name + " から退出しました。" 
+            await client.send_message(vcchannel, outtext) 
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-    
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-@client.event
-async def on_voice_state_update(before, after):
-    print("ボイスチャンネルで変化がありました")
-
-if(before.voice_channel is None):
-    pretime_dict[after.name] = datetime.datetime.now()
-elif(after.voice_channel is None):
-    duration_time = pretime_dict[before.name] - datetime.datetime.now()
-    duration_time_adjust = int(duration_time.total_seconds()) * -1
-
-reply_channel_name = "time"
-reply_channel = [channel for channel in before.server.channels if channel.name == reply_channel_name][0]
-reply_text = after.name + "　が　" + before.voice_channel.name + "　から抜けました。　通話時間：" + str(duration_time_adjust) + "秒"
-
-await client.send_message(reply_channel, reply_text)
-
-bot.run(token)
+client.run(token)
