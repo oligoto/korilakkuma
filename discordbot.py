2f8fc060b50a8f154e4332091bd691aeb76b1e05
oligoto <61726719+oligoto@users.noreply.github.com>
2020-03-03 17:56:49 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -8,7 +8,7 @@ client = discord.Client()
 @client.event
 async def on_voice_state_update(member, before, after):
     now = datetime.utcnow() + timedelta(hours=9)
-    alert_channel = client.get_channel(general)
+    alert_channel = client.get_channel(684293450954178585)
     if before.channel is None:
         msg = f'{now:%m/%d-%H:%M} に {member.name} が {after.channel.name} に参加しました。'
         await alert_channel.send(msg)
