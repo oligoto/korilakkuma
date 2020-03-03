e9b23d9a829e74a656a315945d09011098f9a591
oligoto <61726719+oligoto@users.noreply.github.com>
2020-03-03 17:32:51 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,15 +5,14 @@ import datetime
 token = os.environ['DISCORD_BOT_TOKEN']
 client = discord.Client()
 
-@client.event 
+@client.event
 async def on_voice_state_update(before, after):
-    if nowtime = datetime.datetime.utcnow()
-nowtime = nowtime + datetime.timedelta(hours=9)
+    if nowtime = nowtime + datetime.timedelta(hours=9)
 nowtime = nowtime.strftime("%m/%d-%H:%M")
 vcchannel = client.get_channel('time')
  
 if(before.voice_channel is None):
-    jointext=nowtime + "に　"+ after.name + "　が　"+ after.voice_channel.name + " に参加しました。"
+    jointext=nowtime + "に　" + after.name + "　が　" + after.voice_channel.name + " に参加しました。"
     await client.send_message(vcchannel, jointext)
 elif(after.voice_channel is None):
     outtext=nowtime + "に " + before.name + "　が　" + before.voice_channel.name + " から退出しました。"
