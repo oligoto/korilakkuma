1ccfcaa9c56efa348a056c7d5df9a0123d0f24d0
oligoto <61726719+oligoto@users.noreply.github.com>
2020-03-03 16:30:57 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -10,22 +10,22 @@ pretime_dict = {}
 
 @client.event
 async def on_voice_state_update(before, after):
-print("ボイスチャンネルで変化がありました")
+    print("ボイスチャンネルで変化がありました")
 
 if((before.voice.self_mute is not after.voice.self_mute) or (before.voice.self_deaf is not after.voice.self_deaf)):
- print("ボイスチャンネルでミュート設定の変更がありました")
+    print("ボイスチャンネルでミュート設定の変更がありました")
 return
 
 if(before.voice_channel is None):
- pretime_dict[after.name] = datetime.datetime.now()
+    pretime_dict[after.name] = datetime.datetime.now()
 elif(after.voice_channel is None):
- duration_time = pretime_dict[before.name] - datetime.datetime.now()
- duration_time_adjust = int(duration_time.total_seconds()) * -1
+    duration_time = pretime_dict[before.name] - datetime.datetime.now()
+    duration_time_adjust = int(duration_time.total_seconds()) * -1
 
 reply_channel_name = "general"
- reply_channel = [channel for channel in before.server.channels if channel.name == reply_channel_name][0]
- reply_text = after.name + "　が　"+ before.voice_channel.name + "　から抜けました。　通話時間：" + str(duration_time_adjust) +"秒"
+    reply_channel = [channel for channel in before.server.channels if channel.name == reply_channel_name][0]
+    reply_text = after.name + "　が　" + before.voice_channel.name + "　から抜けました。　通話時間：" + str(duration_time_adjust) + "秒"
 
- await client.send_message(reply_channel ,reply_text)
+    await client.send_message(reply_channel ,reply_text)
 
-client.run("token")#ボットのトークン
+client.run("token") #ボットのトークン
